7a26ca40a515919b380c323cbcb2ea8dfe4cb2a8
Jodu555 <Jodu505@gmail.com>
2021-12-05 13:29:04 +0100

    added node-fetch

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,18 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
+
+const URL = 'http://51.195.60.60:8006/api2/json'
 
 async function run() {
-
+    const response = await fetch(URL + '/access/ticket', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: 'root@pam',
+            password: '8952464b73e9021544e7b6297abb74bf'
+        })
+    });
+    console.log(response);
 }
+
+// curl -k -d  https://51.195.60.60:8006/api2/json/access/ticket
 
 run();
