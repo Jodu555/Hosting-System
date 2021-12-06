679fc27829fe9343f310cb7c4aea058be2e06c14
Jodu555 <Jodu505@gmail.com>
2021-12-06 13:49:31 +0100

    Updated and edited things

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,17 +15,23 @@ async function run() {
 
     // console.log(await node.information());
 
-    const template = node.getVM(100);
-    await template.clone({ newid: 101 });
+    // const template = node.getVM(100);
+    await template.clone({
+        newid: 101,
+        full: true // So the storage is independent
+    });
 
     const newVM = node.getVM(101);
+    // await newVM.configurate({
+    //     name: 'Test-API',
+    //     // ide2: 'local:iso/debian-10.11.0-amd64-netinst.iso',
+    //     cores: 4,
+    //     sockets: 4,
+    //     memory: 6024,
+    //     net0: 'virtio=02:00:00:01:c6:6b,bridge=vmbr0,firewall=1',
+    //     // scsi0: 'local:32,format=qcow2'
+    // })
     await newVM.configurate({
-        name: 'Test-API',
-        ide2: 'local:iso/debian-10.11.0-amd64-netinst.iso',
-        cores: 4,
-        sockets: 4,
-        memory: 6024,
-        net0: 'virtio=02:00:00:01:c6:6b,bridge=vmbr0,firewall=1',
         scsi0: 'local:32,format=qcow2'
     })
 
