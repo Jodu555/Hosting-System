3a223e80ad81f3a34e3bb9e2d6597d8ee02db6f7
Jodu555 <Jodu505@gmail.com>
2021-12-21 07:53:43 +0100

    Added only one function fo the role authorization

--- Backend/src/routes/packages/index.js
+++ Backend/src/routes/packages/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const controller = require('./controller');
-const authManager = require('../../utils/authManager');
 const router = express.Router();
-const { isPermitted } = require('../../utils/roleManager')
+const { roleAuthorization } = require('../../utils/roleManager')
 
 router.get('/', controller.list); //List Packages
 
-router.post('/', authManager.authenticationFull((user) => isPermitted(user, 'admin')), controller.create); //Create Package
+router.post('/', roleAuthorization('admin'), controller.create); //Create Package
 
 
 
--- Backend/src/utils/roleManager.js
+++ Backend/src/utils/roleManager.js
@@ -1,8 +1,15 @@
+const authManager = require('./authManager');
+
 const isPermitted = (user, role) => {
     //TODO: Do there some permission stuff
     return user.UUID == 'a6e7ba88-53eb-4233-8789-691741583b3a';
 }
 
+const roleAuthorization = (role) => {
+    return authManager.authenticationFull((user) => isPermitted(user, role));
+}
+
 module.exports = {
-    isPermitted
+    isPermitted,
+    roleAuthorization
 }
