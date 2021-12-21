3c36b911faeb78ada1a236e48b6859ec56f82f2e
Jodu555 <Jodu505@gmail.com>
2021-12-21 07:50:54 +0100

    added a roleManager for later usage

--- Backend/src/routes/packages/index.js
+++ Backend/src/routes/packages/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const controller = require('./controller');
 const authManager = require('../../utils/authManager');
 const router = express.Router();
+const { isPermitted } = require('../../utils/roleManager')
 
 router.get('/', controller.list); //List Packages
-router.post('/', authManager.authenticationFull((user) => {
-    return user.UUID == 'a6e7ba88-53eb-4233-8789-691741583b3a';
-}), controller.create); //Create Package
+
+router.post('/', authManager.authenticationFull((user) => isPermitted(user, 'admin')), controller.create); //Create Package
 
 
 
---
+++ Backend/src/utils/roleManager.js
@@ -0,0 +1,8 @@
+const isPermitted = (user, role) => {
+    //TODO: Do there some permission stuff
+    return user.UUID == 'a6e7ba88-53eb-4233-8789-691741583b3a';
+}
+
+module.exports = {
+    isPermitted
+}
