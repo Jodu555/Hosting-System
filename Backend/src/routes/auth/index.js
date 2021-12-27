c5432a590060744c7b3eb6073feda5d91d5e3d9f
Jodu555 <Jodu505@gmail.com>
2021-12-27 07:02:10 +0100

    Quickly added a auth info route

--- Backend/src/routes/auth/index.js
+++ Backend/src/routes/auth/index.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
 router.post('/register', controller.register);
 router.post('/login', controller.login);
 router.get('/logout', authManager.authentication, controller.logout);
+router.get('/info', authManager.authentication, (req, res, next) => { res.json(req.credentials.user) });
 
 module.exports = {
     router,
