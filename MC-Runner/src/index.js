29b4fe79f48b5866f6c2e4f66a899024cff95145
Jodu555 <Jodu505@gmail.com>
2022-03-10 16:08:08 +0100

    Expanded the testing and noticed the system works till now fine

--- MC-Runner/src/index.js
+++ MC-Runner/src/index.js
@@ -1,11 +1,18 @@
 const FTPServer = require('./FTPServer.js')
+const path = require('path')
+
 
 const server = new FTPServer('127.0.0.1', '7878')
 server.onAuthenticate((username, password) => {
     if (username == 'test' && password == '123') {
         return {
             success: true,
-            root: process.cwd(),
+            root: path.join(process.cwd(), 'data', 'usr_1'),
+        }
+    } else if (username == 'asdf' && password == '456') {
+        return {
+            success: true,
+            root: path.join(process.cwd(), 'data', 'usr_2'),
         }
     } else {
         return {
