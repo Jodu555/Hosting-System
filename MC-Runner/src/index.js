bdc16632f3df0601a4e1a5afafb158bec42bd966
Jodu555 <Jodu505@gmail.com>
2022-03-10 13:24:33 +0100

    Tested more and more

--- MC-Runner/src/index.js
+++ MC-Runner/src/index.js
@@ -1,13 +1,25 @@
 const FtpSrv = require('ftp-srv');
-const ftpServer = new FtpSrv({ url: 'ftp://localhost:7878' });
+const ftpServer = new FtpSrv({
+    url: 'ftp://localhost:7878',
+    whitelist: ['PWD', 'TYPE', 'PASV', 'USER', 'PASS'],
+    file_format: (...data) => {
+        console.log(1337, ...data);
+        return null;
+    }
+});
 const path = require('path');
 
 ftpServer.on('login', ({ connection: { id }, username, password }, resolve, reject) => {
     console.log(id, username, password);
     if (username == 'test' && password == 123) {
+        const root = path.join(__dirname, '..', 'node_modules')
+        console.log(root);
         return resolve({
-            root: '/',
-            // cwd: '../node_modules/',
+            root,
+            // root: '../node_modules',
+            cwd: '/',
+            whitelist: ['PWD', 'TYPE', 'PASV'],
+            greeting: 'Welcome'
         });
     } else {
         return reject({});
