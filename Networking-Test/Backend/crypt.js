6c9fdb9b4dd691c6a6429594ea8578096f80b416
Jodu555 <Jodu505@gmail.com>
2022-03-18 16:32:40 +0100

    thouught of a krypto function to use with the authentication

---
+++ Networking-Test/Backend/crypt.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-ctr';
+const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
+const iv = crypto.randomBytes(16);
+
+const encrypt = (text) => {
+
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+
+    return {
+        iv: iv.toString('hex'),
+        content: encrypted.toString('hex')
+    };
+};
+
+const decrypt = (hash) => {
+
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+
+    return decrpyted.toString();
+};
+
+console.time('crypt');
+const hash = encrypt(JSON.stringify({ type: 'Relay', ip: '127.0.0.1' }));
+console.log(hash);
+
+const auth = JSON.parse(decrypt(hash));
+console.log(auth);
+console.timeEnd('crypt'); // 6-7ms
+
+
+module.exports = {
+    encrypt,
+    decrypt
+};
