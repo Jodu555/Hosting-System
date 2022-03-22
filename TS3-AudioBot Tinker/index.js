c1b18e56aa238aabd537d561bbc456c0c6c5c1ab
Jodu555 <Jodu505@gmail.com>
2022-03-22 09:09:35 +0100

    Got the basic call thing working

--- TS3-AudioBot Tinker/index.js
+++ TS3-AudioBot Tinker/index.js
@@ -1,1 +1,23 @@
+require('dotenv').config();
 const axios = require('axios');
+const API_URL = process.env.API_URL + '/api';
+
+const createNewBot = (name, address, autoplay) => {
+    call(`/settings/copy/default/${name}`)
+}
+
+
+
+async function call(url = '') {
+    try {
+        const authStr = btoa(`${process.env.API_USERNAME}:${process.env.API_TOKEN}`);
+        const response = await axios.get(API_URL + url, {
+            headers: { 'Authorization': `Basic ${authStr}` }
+        });
+        console.log(response.status);
+    } catch (error) {
+        console.error('Got Call error', url, error);
+    }
+}
+
+call()
