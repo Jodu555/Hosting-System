d30ac3a3e9e4c4254a37398991a0b85f607e3e89
Jodu555 <Jodu505@gmail.com>
2022-03-22 09:15:10 +0100

    Fixes

--- TS3-AudioBot Tinker/index.js
+++ TS3-AudioBot Tinker/index.js
@@ -2,22 +2,25 @@ require('dotenv').config();
 const axios = require('axios');
 const API_URL = process.env.API_URL + '/api';
 
-const createNewBot = (name, address, autoplay) => {
-    call(`/settings/copy/default/${name}`)
+async function createNewBot(name, address, autoplay) {
+    await call(`/settings/copy/default/${name}`);
+    await call(`/settings/bot/set/${name}/connect.identity.key/`);
+    await call(`/settings/bot/reload/${name}`);
+    await call(`/bot/connect/template/${name}`);
+    console.log('Finished');
 }
 
-
-
 async function call(url = '') {
+    url = API_URL + url;
     try {
         const authStr = btoa(`${process.env.API_USERNAME}:${process.env.API_TOKEN}`);
-        const response = await axios.get(API_URL + url, {
+        const response = await axios.get(url, {
             headers: { 'Authorization': `Basic ${authStr}` }
         });
-        console.log(response.status);
+        console.log({ status: response.status, data: response.data });
     } catch (error) {
         console.error('Got Call error', url, error);
     }
 }
 
-call()
+createNewBot('apifinn');
