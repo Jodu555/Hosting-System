497a54a18d22bffce28aba305c3085d1515c5619
Jodu555 <Jodu505@gmail.com>
2022-03-22 09:40:25 +0100

    added the default channel property

--- TS3-AudioBot Tinker/index.js
+++ TS3-AudioBot Tinker/index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const axios = require('axios');
 const API_URL = process.env.API_URL + '/api';
 
-async function createNewBot(name, address, autoplayUrl = '') {
+async function createNewBot(name, address, autoplayUrl = '', defaultChannel = null) {
     await call(`/settings/copy/default/${name}`);
     //Change the bot settings!
     await call(`/settings/bot/set/${name}/connect.address/${address}`);
     await call(`/settings/bot/set/${name}/connect.name/${name}`);
+    if (defaultChannel != null)
+        await call(`/settings/bot/set/${name}/connect.channel/${encUri(`/${defaultChannel}`)}`);
+
     await call(`/settings/bot/set/${name}/events.onconnect/!play ${autoplayUrl}`);
     await call(`/settings/bot/set/${name}/connect.identity.key/`);
 
@@ -32,13 +35,15 @@ async function call(url = '') {
 
 const RadioBots = {
     'SpreeRadio': 'https://stream.spreeradio.de/spree-live/mp3-256/konsole/',
-    'RTL': '',
-    'Energy': '',
+    'RTL': 'http://stream.104.6rtl.com/rtl-live/mp3-192/radio-browser.info/',
+    'Energy': 'http://cdn.nrjaudio.fm/adwz1/de/33019/mp3_128.mp3',
     'KissFM': '',
     'JamFM': ''
 };
 
 Object.entries(RadioBots).forEach(([k, v]) => {
     console.log(k, v);
-    createNewBot(k, 'rooti.jodu555.de', encodeURIComponent(v));
+    createNewBot(k, 'rooti.jodu555.de', encUri(v));
 });
+
+const encUri = u => encodeURIComponent(u);
