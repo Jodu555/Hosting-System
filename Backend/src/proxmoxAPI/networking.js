f2197bc14cc8a5827b9c3b2551bb431fb7e18ad5
Jodu555 <Jodu505@gmail.com>
2022-03-23 12:33:57 +0100

    Improved the error handling in the proxmox api axios thingi

--- Backend/src/proxmoxAPI/networking.js
+++ Backend/src/proxmoxAPI/networking.js
@@ -7,60 +7,48 @@ const httpsAgent = new https.Agent({
 
 
 async function get(url, headers) {
-    // const response = await fetch(url, {
-    //     method: 'GET',
-    //     headers,
-    //     agent: httpsAgent,
-    // });
-    const response = await axios.get(url, { httpsAgent, headers })
-    return response;
+    try {
+        const response = await axios.get(url, { httpsAgent, headers })
+        return response;
+    } catch (error) {
+        console.log(error, url, headers);
+    }
 }
 
 async function post(url, data, headers = {}) {
-    // const response = await fetch(url, {
-    //     method: 'POST',
-    //     headers: {
-    //         ...headers,
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data),
-    //     agent: httpsAgent,
-    // });
-    const response = await axios.post(url, data, {
-        httpsAgent,
-        headers,
-    })
-    return response;
+    try {
+        const response = await axios.post(url, data, {
+            httpsAgent,
+            headers,
+        })
+        return response;
+    } catch (error) {
+        console.log(error, url, data, headers);
+    }
 }
 
 async function put(url, data, headers = {}) {
-    // const response = await fetch(url, {
-    //     method: 'PUT',
-    //     headers: {
-    //         ...headers,
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data),
-    //     agent: httpsAgent,
-    // });
-    const response = await axios.put(url, data, {
-        httpsAgent,
-        headers
-    })
-    return response;
+    try {
+        const response = await axios.put(url, data, {
+            httpsAgent,
+            headers
+        })
+        return response;
+    } catch (error) {
+        console.log(error, url, data, headers);
+    }
 }
 
 async function del(url, headers = {}) {
-    // const response = await fetch(url, {
-    //     method: 'DELETE',
-    //     headers,
-    //     agent: httpsAgent,
-    // });
-    const response = await axios.delete(url, {
-        httpsAgent,
-        headers
-    });
-    return response;
+    try {
+        const response = await axios.delete(url, {
+            httpsAgent,
+            headers
+        });
+        return response;
+    } catch (error) {
+        console.log(error, url, headers);
+    }
 }
 
 module.exports = {
