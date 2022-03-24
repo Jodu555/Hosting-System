5c1ed63f68ada8ecdf3270649c1cae26fe0f5efd
Jodu555 <Jodu505@gmail.com>
2022-03-24 17:40:18 +0100

    Update

--- Relay/src/index.js
+++ Relay/src/index.js
@@ -6,11 +6,6 @@ const Config = require('./Config');
 
 const socket = io('http://localhost:3100');
 
-// const authValues = {
-//     iv: '3f5e2acb93a81395983af8ae86e23f5d',
-//     content: '6c50b3224e05f0123d782c422ed9c855d7b72f2d77f7095b2dc9bcceca0de41bd7'
-// };
-
 const relay = new Relay();
 
 const config = new Config({
