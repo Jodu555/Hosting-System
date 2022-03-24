eb57836502d7a8075478773cb58ae6f80587c956
Jodu555 <Jodu505@gmail.com>
2022-03-24 18:28:11 +0100

    Logger enhancement

--- Relay/src/index.js
+++ Relay/src/index.js
@@ -3,36 +3,40 @@ const RelayEntity = require('./RelayEntity');
 
 const { io } = require("socket.io-client");
 const Config = require('./utils/Config');
+const Logger = require('./utils/Logger');
 
-const config = new Config({
-    RELAY_IP: 'PUT HERE THE EXTERNAL RELAY IP | So the IP of this machine',
-    BACKEND_IP: 'PUT HERE THE IP OF YOUR BACKEND SERVER',
-    authValues: {
-        iv: 'PUT HERE YOUR IV',
-        content: 'PUT HERE YOUR CONTENT'
-    }
-});
+const logger = new Logger();
+logger.fatal('123')
 
-const socket = io(config.get().BACKEND_IP);
+// const config = new Config({
+//     RELAY_IP: 'PUT HERE THE EXTERNAL RELAY IP | So the IP of this machine',
+//     BACKEND_IP: 'PUT HERE THE IP OF YOUR BACKEND SERVER',
+//     authValues: {
+//         iv: 'PUT HERE YOUR IV',
+//         content: 'PUT HERE YOUR CONTENT'
+//     }
+// });
 
-const relay = new Relay();
+// const socket = io(config.get().BACKEND_IP);
 
-socket.on('connect', () => {
-    socket.emit('auth', config.get().authValues);
-});
+// const relay = new Relay();
 
-socket.on('auth-success', () => {
-    console.log('Got Authentication-Success! The System is now ready to work');
-});
+// socket.on('connect', () => {
+//     socket.emit('auth', config.get().authValues);
+// });
 
-socket.on('auth-error', ({ message }) => {
-    console.log('Got Authentication-Error:', message);
-});
+// socket.on('auth-success', () => {
+//     console.log('Got Authentication-Success! The System is now ready to work');
+// });
+
+// socket.on('auth-error', ({ message }) => {
+//     console.log('Got Authentication-Error:', message);
+// });
 
 
-socket.on('rl-conn-open', ({ extPort, intPort }) => {
-    console.log('GOT openConnection', extPort, intPort);
-    // const re = new RelayEntity('164.132.170.199', 25518, '127.0.0.1', 10337);
-    // relay.insert(re);
-});
+// socket.on('rl-conn-open', ({ extPort, intPort }) => {
+//     console.log('GOT openConnection', extPort, intPort);
+//     // const re = new RelayEntity('164.132.170.199', 25518, '127.0.0.1', 10337);
+//     // relay.insert(re);
+// });
 
--- Relay/src/utils/Logger.js
+++ Relay/src/utils/Logger.js
@@ -1,16 +1,38 @@
 class Logger {
     constructor(file) {
         this.file = file;
+        this.level = -1;
         this.levels = {
-            fatal: 0,
-            error: 1,
-            warn: 2,
-            info: 3,
-            debug: 4
+            fatal: { value: 4, name: 'Fatal' },
+            error: { value: 3, name: 'Error' },
+            warn: { value: 2, name: 'Warn' },
+            info: { value: 1, name: 'Info' },
+            debug: { value: 0, name: 'Debug' }
         }
     }
     setLevel(level) {
-
+        Number.isInteger(level) ?
+            this.level = level :
+            this.level = this.levels[level].value;
+    }
+    deepLog(level, ...args) {
+        if (this.level > level)
+            console.log(``,);
+    }
+    fatal() {
+        deepLog(this.levels.fatal.value);
+    }
+    error() {
+        deepLog(this.levels.error.value);
+    }
+    warn() {
+        deepLog(this.levels.warn.value);
+    }
+    info() {
+        deepLog(this.levels.info.value);
+    }
+    debug() {
+        deepLog(this.levels.debug.value);
     }
 }
 
