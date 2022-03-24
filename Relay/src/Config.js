f4d0aaf00b5f14c29602ca442a9bf3d054109728
Jodu555 <Jodu505@gmail.com>
2022-03-24 17:34:45 +0100

    Fully added the config.js

--- Relay/src/Config.js
+++ Relay/src/Config.js
@@ -3,23 +3,28 @@ const path = require('path');
 
 
 class Config {
-    constructor(config, path = '') {
+    /**
+     * @param  {Object} config The initial config
+     * @param  {String} path='' The process.cwd() under path for example configs/
+     * @param  {String} name='config.json' The configuration file
+     */
+    constructor(config, path = '', name = 'config.json') {
         this.config = config;
         this.path = path;
-        this.load();
+        this.name = name;
+        if (fs.existsSync(this.getPath()))
+            this.load();
         this.save();
     }
 
     getPath() {
-        return path.join(__dirname, this.path, 'config.json');
+        return path.join(process.cwd(), this.path, this.name);
     }
-
     load() {
-        this.config = 
+        this.config = JSON.parse(fs.readFileSync(this.getPath(), 'utf-8'));
     }
-
     save() {
-
+        fs.writeFileSync(this.getPath(), JSON.stringify(this.config, null, 3), 'utf-8');
     }
 
 }
