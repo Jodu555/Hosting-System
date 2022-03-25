6f0de4d534e87b4f82ad4fb936d294dbfe59d18c
Jodu555 <Jodu505@gmail.com>
2022-03-25 12:28:04 +0100

    Moved the logger to its own package and repo

--- Relay/src/index.js
+++ Relay/src/index.js
@@ -3,19 +3,7 @@ const RelayEntity = require('./RelayEntity');
 
 const { io } = require("socket.io-client");
 const Config = require('./utils/Config');
-const Logger = require('./utils/Logger');
 
-const path = require('path');
-
-
-const logger = new Logger(path.join(process.cwd(), 'latest.log'));
-logger.setLevel(-1);
-
-logger.fatal('123: fatal');
-logger.error('123: error');
-logger.warn('123: warn');
-logger.info('123: info');
-logger.debug('123: debug');
 
 // const config = new Config({
 //     RELAY_IP: 'PUT HERE THE EXTERNAL RELAY IP | So the IP of this machine',
