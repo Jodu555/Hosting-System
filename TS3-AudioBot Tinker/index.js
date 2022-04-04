dca865eec2d2e7c209a146c9696894cdbeca88c1
Jodu555 <Jodu505@gmail.com>
2022-04-04 15:58:33 +0200

    added command ideas

--- TS3-AudioBot Tinker/index.js
+++ TS3-AudioBot Tinker/index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const API_URL = process.env.API_URL + '/api';
 
 //TODO: Implement the command manager
+//TODO: Commands: [reload, create]
 
 const encUri = u => encodeURIComponent(u);
 
