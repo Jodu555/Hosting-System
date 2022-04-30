00160e24fbc79c488852b6183ea825a64803b6ca
Finn <37829341+Jodu555@users.noreply.github.com>
2022-04-30 23:24:14 +0200

    added a todo

--- Backend/src/routes/manage/kvm.js
+++ Backend/src/routes/manage/kvm.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
+//TODO: Think maybe of other routes or also to implement these routes into the socket handling
+
 router.get('/start');  //To Start the KVM
 router.get('/stop');  //To Stop the KVM
 router.get('/restart'); // To restart the KVM
