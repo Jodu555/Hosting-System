6be6e211b87121309762db42218cb2ed9a31f98e
Finn <37829341+Jodu555@users.noreply.github.com>
2022-04-30 23:23:30 +0200

    documented the routes

--- Backend/src/routes/manage/kvm.js
+++ Backend/src/routes/manage/kvm.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/start');
-router.get('/stop');
-router.get('/restart');
-router.get('/status');
+router.get('/start');  //To Start the KVM
+router.get('/stop');  //To Stop the KVM
+router.get('/restart'); // To restart the KVM
+router.get('/status'); // To get status information about the KVM
 
 
 
