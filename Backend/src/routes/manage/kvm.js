8e6af29b45fdfbd9e35596318309285dd16d45c2
Jodu555 <Jodu505@gmail.com>
2022-05-04 18:51:41 +0200

    added some stuff to get to the vm

--- Backend/src/routes/manage/kvm.js
+++ Backend/src/routes/manage/kvm.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const { getProxmoxApi } = require('../../utils/utils');
 const router = express.Router();
 
 
 //TODO: Think maybe of other routes or also to implement these routes into the socket handling
 //TODO: Maybe implement some type of backup routes
 
-router.get('/start', (req, res, next) => { //To Start the KVM
+router.get('/start', async (req, res, next) => { //To Start the KVM
     const UUID = req.params.ID;
+    const node = (await getProxmoxApi()).getNode(process.env.DEFAULT_NODE);
+
+    const vm = node.getVM(VM_ID);
 });
 router.get('/stop');  //To Stop the KVM
 router.get('/restart'); // To restart the KVM
