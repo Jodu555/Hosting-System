0f36e93124658685782d81b4a6e1efdca77fa9bd
Jodu555 <Jodu505@gmail.com>
2022-05-05 00:16:45 +0200

    added todo stuff

--- Backend/src/routes/manage/index.js
+++ Backend/src/routes/manage/index.js
@@ -3,7 +3,7 @@ const kvm_controller = require('./kvm');
 const router = express.Router();
 
 router.use('/kvm/:ID', (req, res, next) => {
-
+    //TODO: put here the code to validate if the vm ID exists && append it into the req object to dont use duplicate code
 }, kvm_controller); // Manage all the KVM Stuff
 
 
