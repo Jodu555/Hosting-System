dfdad047465675e096eb2da88494b4f1faa88515
Jodu555 <Jodu505@gmail.com>
2022-05-05 22:46:35 +0200

    restructured the code + added the ownership restriction

--- Backend/src/routes/manage/index.js
+++ Backend/src/routes/manage/index.js
@@ -6,18 +6,25 @@ const router = express.Router();
 const database = Database.getDatabase();
 
 router.use('/kvm/:ID', async (req, res, next) => {
-    //TODO: put here the code to validate if the vm ID exists && append it into the req object to dont use duplicate code
-
-
     const UUID = req.params.ID;
     const node = (await getProxmoxApi()).getNode(process.env.DEFAULT_NODE);
     const kvm_service = await database.get('kvm_package_services').getOne({ UUID });
+    const product = database.get('products').getOne({ UUID: kvm_service.product_UUID });
 
-    if (kvm_service) {
-
-    } else {
-        next(new Error('You dont own any kvm service with that ID'));
+    if (product.account_UUID !== req.credentials.user.UUID) {
+        next(new Error('You dont own this package'));
+        return;
     }
+    if (!kvm_service) {
+        next(new Error('You dont own any kvm service with that ID'));
+        return;
+    }
+
+    req.credentials.kvm = {
+        node,
+        service: kvm_service,
+        product
+    };
 }, kvm_controller); // Manage all the KVM Stuff
 
 
