2abea09eb4608d4f2fa9cdb2f4c10276922e9579
Jodu555 <Jodu505@gmail.com>
2022-05-05 22:39:40 +0200

    added the pre routing

--- Backend/src/routes/manage/index.js
+++ Backend/src/routes/manage/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const kvm_controller = require('./kvm');
+const { Database } = require('@jodu555/mysqlapi');
+const { getProxmoxApi } = require('../../utils/utils');
 const router = express.Router();
+const database = Database.getDatabase();
 
-router.use('/kvm/:ID', (req, res, next) => {
+router.use('/kvm/:ID', async (req, res, next) => {
     //TODO: put here the code to validate if the vm ID exists && append it into the req object to dont use duplicate code
+
+
+    const UUID = req.params.ID;
+    const node = (await getProxmoxApi()).getNode(process.env.DEFAULT_NODE);
+    const kvm_service = await database.get('kvm_package_services').getOne({ UUID });
+
+    if (kvm_service) {
+
+    } else {
+        next(new Error('You dont own any kvm service with that ID'));
+    }
 }, kvm_controller); // Manage all the KVM Stuff
 
 
